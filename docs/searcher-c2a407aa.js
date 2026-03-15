5dec5cc3bf6cbf184a35c40d79d2252b2d9fb42c
Corvin Gröning <114770097+cgroening@users.noreply.github.com>
2026-03-15 09:51:28 +0100

    Aktualisiere HTML mit mdbook

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e3edc862.js',
+                path_to_root + 'searchindex-3a8d677a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
